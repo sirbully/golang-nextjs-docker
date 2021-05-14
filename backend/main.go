a77020b1c5994f64f22a3a3d5e7cde93aad91404
Krizza Bullecer <krizzabullecer@gmail.com>
2021-05-14 22:06:29 +0800

    refactor: use gorilla handler CORS middleware

--- backend/main.go
+++ backend/main.go
@@ -9,8 +9,9 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/sirbully/golang-nextjs-docker/handlers"
+	"github.com/sirbully/golang-nextjs-docker/funchandlers"
 )
 
 func main() {
@@ -20,19 +21,25 @@ func main() {
 	r := mux.NewRouter()
 
 	// initialize message handler and register the handlers to mux
-	mh := handlers.NewMessages(l)
+	mh := funchandlers.NewMessages(l)
 	r.HandleFunc("/healthcheck", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Write([]byte("Healthy"))
 	})
-	r.HandleFunc("/api/messages", mh.GetMessages).Methods(http.MethodGet, http.MethodOptions)
-	r.HandleFunc("/api/messages", mh.CreateMessage).Methods(http.MethodPost, http.MethodOptions)
-	r.HandleFunc("/api/messages/{id:[0-9]+}", mh.UpdateMessage).Methods(http.MethodPut, http.MethodOptions)
-	r.HandleFunc("/api/messages/{id:[0-9]+}", mh.DeleteMessage).Methods(http.MethodDelete, http.MethodOptions)
+	r.HandleFunc("/api/messages", mh.GetMessages).Methods(http.MethodGet)
+	r.HandleFunc("/api/messages", mh.CreateMessage).Methods(http.MethodPost)
+	r.HandleFunc("/api/messages/{id:[0-9]+}", mh.DeleteMessage).Methods(http.MethodDelete)
+	r.HandleFunc("/api/messages/{id:[0-9]+}", mh.UpdateMessage).Methods(http.MethodPut)
+
+	// cors middleware
+	methods := handlers.AllowedMethods([]string{"OPTIONS", "DELETE", "GET", "HEAD", "POST", "PUT"})
+	origins := handlers.AllowedOrigins([]string{"*"})
+	headers := handlers.AllowedHeaders([]string{"Content-Type"})
+	handler := handlers.CORS(methods, origins, headers)(r)
 
 	// create and start server
 	s := &http.Server{
 		Addr:     ":9090", // bind address
-		Handler:  r,       // default handler
+		Handler:  handler, // default handler
 		ErrorLog: l,       // logger for the server
 	}
 	go func() {
