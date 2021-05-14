fa3fb843bfeed4ad7670fabc1784f32d8d5978ac
Krizza Bullecer <krizzabullecer@gmail.com>
2021-05-14 16:02:24 +0800

    feat: integrate react query

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,19 @@
 import Head from "next/head"
+import { QueryClient, QueryClientProvider } from "react-query"
+import { ReactQueryDevtools } from "react-query/devtools"
 
-import "tailwindcss/tailwind.css"
+import "../styles/globals.scss"
+
+const queryClient = new QueryClient()
 
 const MyApp = ({ Component, pageProps }) => (
-  <>
+  <QueryClientProvider client={queryClient}>
     <Head>
       <link rel="shortcut icon" href="/favicon.png" />
     </Head>
     <Component {...pageProps} />
-  </>
+    {process.env.NODE_ENV !== "production" && <ReactQueryDevtools />}
+  </QueryClientProvider>
 )
 
 export default MyApp
